4ec760840d8e35325359f99a1be8bbd4e7e34de1
catdotjs <catdotjs@gmail.com>
2023-09-27 06:19:43 +0300

    Moved get net versions to a class and added async func

--- Scripts/DotNetInfo.cs
+++ Scripts/DotNetInfo.cs
@@ -1,6 +1,28 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CliWrap;
+using CliWrap.Buffered;
 
 namespace Lyre;
 
 static class DotNetInfo{
-    
+    /// <summary>
+    /// Gets current SDK's and writes the versions to ComboBox
+    /// </summary>
+    /// <returns> Task<List<string>> </returns>
+    public static async Task<List<string>> GetVersions(){
+        // example output:(oldest to newest version)
+        // 6.0.414 [/sdk/path/]
+        // 7.0.400 [/sdk/path/]
+        BufferedCommandResult resultBuffer = await Cli.Wrap("dotnet").WithArguments("--list-sdks").ExecuteBufferedAsync();
+        string[] versionStringArray = resultBuffer.StandardOutput.TrimEnd().Split("\n");
+
+        // Reverse list so newer SDK's are upper
+        versionStringArray = versionStringArray.Reverse().ToArray();
+
+        List<string> versions = versionStringArray.Select(x=>x[0]).Distinct().Select(x=>$".NET {x}.0").ToList();
+        return versions;
+    }
 }
--- ViewModels/CreateProjectVM.cs
+++ ViewModels/CreateProjectVM.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using CliWrap;
-using CliWrap.Buffered;
 using ReactiveUI;
-
-#pragma warning disable CS4014
 
 namespace Lyre.ViewModels;
 public class CreateProjectVM : ViewModelBase {
@@ -18,15 +13,17 @@ public class CreateProjectVM : ViewModelBase {
     }
     
     // DotNet Versions
-    private readonly static List<string> versions = new();
-    public static List<string> Versions { get{
-        GetDotNetVersions();
-        return versions;
-    }}
+    private static List<string> versions = new();
+    public static List<string> Versions { get => versions;}
     
     // Init
     public CreateProjectVM(IWindow iwindow){
         usedWindow = iwindow;
+        AsyncFunctions().GetAwaiter().GetResult();
+    }
+
+    public async Task AsyncFunctions(){
+        versions = await DotNetInfo.GetVersions();
     }
 
     /// <summary>
@@ -35,26 +32,5 @@ public class CreateProjectVM : ViewModelBase {
     /// <returns> void/Task </returns>
     public async Task ChooseProjectPath(){
         ProjectPath = (await FileSystem.GetFolderPath(usedWindow.Current))[0];
-    }
-    
-    /// <summary>
-    /// Gets current SDK's and writes the versions to ComboBox
-    /// </summary>
-    /// <returns> void/Task </returns>
-    private static async Task GetDotNetVersions(){
-        // example output:(oldest to newest version)
-        // 6.0.414 [/sdk/path/]
-        // 7.0.400 [/sdk/path/]
-        BufferedCommandResult resultBuffer = await Cli.Wrap("dotnet").WithArguments("--list-sdks").ExecuteBufferedAsync();
-        string[] versionStringArray = resultBuffer.StandardOutput.TrimEnd().Split("\n");
-        
-        foreach(string version in versionStringArray.Reverse<string>()){
-            // Style it for ComboBox
-            string versionNumber =$".NET {version[0]}.0";
-
-            if(!versions.Contains(versionNumber)){
-                versions.Add(versionNumber);
-            }
-        }
     }
 }
